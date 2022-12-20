508c75f59e6ab706c8b1f79f259fd2dff7956b30
huangcpp <hjp1279@outlook.com>
2022-12-21 00:13:17 +0800

    update

--- main.c
+++ main.c
@@ -1,53 +1,55 @@
 #include <stdio.h>
-struct char_con{
+
+struct char_con {
     char c;
     unsigned times;
 };
 typedef struct char_con char_con;
+
 int main() {
     char x;
     char_con charCon[200];
-    unsigned count=0;
+    unsigned count = 0;
     //输入字符
-    scanf("%c",&x);
-    while (x!=10&&x!=13){
+    scanf("%c", &x);
+    while (x != 10 && x != 13) {
         int i;
         for (i = 0; i < count; ++i) {
-            if(x==charCon[i].c){
+            if (x == charCon[i].c) {
                 charCon[i].times++;
                 break;
             }
         }
-        if(i==count){
-            charCon[i].c=x;
-            charCon[i].times=1;
+        if (i == count) {
+            charCon[i].c = x;
+            charCon[i].times = 1;
             count++;
         }
-        scanf("%c",&x);
+        scanf("%c", &x);
     }
     //通过指针排序
     char_con *p[count];
     for (int i = 0; i < count; ++i) {//用指针指向它们
-        p[i]=charCon+i;
+        p[i] = charCon + i;
     }
     //冒泡排序
-    for (int i = 0; i < count-1; ++i) {
+    for (int i = 0; i < count - 1; ++i) {
         for (int j = 0; j < count - i - 1; ++j) {
-            if(p[j]->times<p[j+1]->times){
-                char_con * t;
-                t=p[j];
-                p[j]=p[j+1];
-                p[j+1]=t;
+            if (p[j]->times < p[j + 1]->times) {
+                char_con *t;
+                t = p[j];
+                p[j] = p[j + 1];
+                p[j + 1] = t;
             }
         }
     }
     int i;
-    for (i = 0; i < count-1; ++i) {
-        printf("%c-%u",p[i]->c,p[i]->times);
-        if((i+1)%4==0)printf("\n");
+    for (i = 0; i < count - 1; ++i) {
+        printf("%c-%u", p[i]->c, p[i]->times);
+        if ((i + 1) % 4 == 0)printf("\n");
         else printf(" ");
     }
-    printf("%c-%u",p[i]->c,p[i]->times);//最后一行行末只换行
+    printf("%c-%u", p[i]->c, p[i]->times);//最后一行行末只换行
     printf("\n");
     return 0;
 }
